4438ed47dbbbc7068cacfef131daec7a4ea4e7e3
MounaGuelsa <mounaguelsa9991@gmail.com>
2024-01-29 15:02:52 +0100

    add patient service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ListePatientsComponent } from './liste-patients/liste-patients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     NavBarComponent,
-    SidebarComponent
+    SidebarComponent,
+    ListePatientsComponent
   ],
   imports: [
     BrowserModule,
