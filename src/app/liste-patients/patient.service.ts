c161597bb3229a11cfc8d75ea04c8544b12bc2a3
MounaGuelsa <mounaguelsa9991@gmail.com>
2024-01-31 21:40:45 +0100

    add patient crud

--- src/app/liste-patients/patient.service.ts
+++ src/app/liste-patients/patient.service.ts
@@ -1,24 +1,44 @@
-
+// patientService.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Patient } from './patient'; 
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class PatientService {
+
   private apiUrl = environment.apiBaseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getPatients(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/patient`);
+  getPatients(): Observable<Patient[]> {
+    return this.http.get<Patient[]>(`${this.apiUrl}/patient`);
   }
 
-  addPatient(patientData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/patient`, patientData);
+  addPatient(patient: Patient): Observable<Patient> {
+    return this.http.post<Patient>(`${this.apiUrl}/patient`, patient);
+  }
+
+  getPatient(id: number): Observable<Patient> {
+    return this.http.get<Patient>(`${this.apiUrl}/patient/${id}`);
+  }
+
+  updatePatient(patient: Patient): Observable<Patient> {
+    return this.http.put<Patient>(`${this.apiUrl}/patient/${patient.patientId}`, patient);
+  }
+
+  deletePatient(id: number): Observable<string> {
+    const isConfirmed = window.confirm('Are you sure you want to delete this patient?');
+    if (isConfirmed) {
+      return this.http.delete(`${this.apiUrl}/patient/${id}`, { responseType: 'text' });
+    } else {
+      return new Observable<string>((observer) => {
+        observer.next('Deletion canceled');
+        observer.complete();
+      });
+    }
   }
 }
-
