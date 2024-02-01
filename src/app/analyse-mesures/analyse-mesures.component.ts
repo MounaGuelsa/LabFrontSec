e7ca0539021f794f5323bcc023638908c3de4335
Ryukiro <hassanth355@gmail.com>
2024-02-01 23:28:29 +0100

    working displaying analyse mesures

--- src/app/analyse-mesures/analyse-mesures.component.ts
+++ src/app/analyse-mesures/analyse-mesures.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalyseMesuresService } from './analyse-mesures.service';
+import { AnalyseMesures } from './analyse-mesures';
 
 @Component({
   selector: 'app-analyse-mesures',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analyse-mesures.component.css']
 })
 export class AnalyseMesuresComponent implements OnInit {
+  allMesures: AnalyseMesures[] = [];
+  mesureById: AnalyseMesures | null = null;
+  idToFetch: number | null = null;
+  showAllMesures: boolean = true;
+  showFetchByIdForm: boolean = false;
 
-  constructor() { }
+  constructor(private analyseMesuresService: AnalyseMesuresService) { }
 
   ngOnInit(): void {
+    this.fetchAllMesures();
   }
 
+  fetchAllMesures(): void {
+    this.analyseMesuresService.getAllMesures().subscribe(mesures => {
+      this.allMesures = mesures;
+      this.showAllMesures = true;
+      this.showFetchByIdForm = false;
+    });
+  }
+  fetchMesureById(): void {
+    if (this.idToFetch !== null) {
+      this.analyseMesuresService.getMesureById(this.idToFetch).subscribe(
+        mesure => {
+          this.mesureById = mesure;
+          this.showAllMesures = false;
+          this.showFetchByIdForm = false;
+        },
+        error => {
+          console.error('Error fetching measure by ID:', error);
+        }
+      );
+    }
+  }
+
+  showFetchById(): void {
+    this.showAllMesures = false;
+    this.showFetchByIdForm = true;
+  }
 }
